9551b51547e3f6bac14afd41621d4fb833249cdc
Carlos Valverde Solera <and_val9@hotmail.com>
2021-02-12 15:56:02 -0600

    Added a local image at GameOverScreen.js

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,14 @@
 import React from "react"
-import {StyleSheet, View, Text} from "react-native";
-import {Button} from "react-native-web";
+import {StyleSheet, View, Text, Button, Image} from "react-native";
 
 
 const GameOverScreen = props => {
     return (
         <View>
             <Text>Game is over with rounds {props.amountOfRounds}</Text>
+            <Image
+                source={require("../assets/splash.png")}
+                style={Styles.image}/>
             <Text>Numbers was {props.selectedNumber}</Text>
             <Button
                 title="Start new game"
@@ -20,6 +22,10 @@ const Styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    image: {
+        width: "80%",
+        height: 300
     }
 })
 
